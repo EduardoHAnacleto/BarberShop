1aba54eafde1ee48135dab54194628efc3d176d1
EduardoHAnacleto <typhonedu@gmail.com>
2026-04-04 00:54:47 +1300

    DbContext created.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -11,10 +11,74 @@ public class AppDbContext : DbContext
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Service> Services { get; set; }
     public DbSet<Worker> Workers { get; set; }
-    //public Status Status { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Service>(entity =>
+        {
+            entity.ToTable("Services");
+
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).HasColumnName("ServiceName");
+            entity.Property(e => e.Price).HasColumnName("ServicePrice");
+            entity.Property(e => e.Description).HasColumnName("ServiceDescription");
+            entity.Property(e => e.Duration).HasColumnName("ServiceDuration");
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.ToTable("Costumers");
+
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).HasColumnName("CostumerName");
+            entity.Property(e => e.DateOfBirth).HasColumnName("CostumerDateOfBirth");
+            entity.Property(e => e.Email).HasColumnName("CostumerEmail");
+            entity.Property(e => e.PhoneNumber).HasColumnName("CostumerPhoneNumber");
+        });
+
+        modelBuilder.Entity<Worker>(entity =>
+        {
+            entity.ToTable("Workers");
+
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).HasColumnName("WorkerName");
+            entity.Property(e => e.DateOfBirth).HasColumnName("WorkerDateOfBirth");
+            entity.Property(e => e.Address).HasColumnName("WorkerAddress");
+            entity.Property(e => e.Position).HasColumnName("WorkerPosition");
+            entity.Property(e => e.PhoneNumber).HasColumnName("WorkerPhoneNumber");
+            entity.Property(e => e.WagePerHour).HasColumnName("WorkerWagePerHour");
+
+            entity.HasMany(e => e.ProvidedServices)
+                  .WithMany()
+                  .UsingEntity(e => e.ToTable("WorkersService"));
+        });
+
+        modelBuilder.Entity<Appointment>(entity =>
+        {
+            entity.ToTable("Appointments");
+
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Service.Id).HasColumnName("AppointmentServiceId");
+            entity.Property(e => e.Status).HasColumnName("AppointmentStatus");
+            entity.Property(e => e.ScheduledFor).HasColumnName("AppointmentScheduledFor");
+            entity.Property(e => e.ExtraDetails).HasColumnName("AppointmentExtraDetails");
+            entity.Property(e => e.CompletedAt).HasColumnName("AppointmentCompletedAt");
+            entity.Property(e => e.Customer.Id).HasColumnName("AppointmentCustomerId");
+            entity.Property(e => e.Worker.Id).HasColumnName("AppointmentWorkerId");
+
+            entity.HasOne(e => e.Customer)
+                  .WithMany()
+                  .HasForeignKey(e => e.Customer.Id)
+                  .OnDelete(DeleteBehavior.NoAction);
+            entity.HasOne(e => e.Worker)
+                  .WithMany()
+                  .HasForeignKey(e => e.Worker.Id)
+                  .OnDelete(DeleteBehavior.NoAction);
+            entity.HasOne(e => e.Service)
+                  .WithMany()
+                  .HasForeignKey(e => e.Service.Id)
+                  .OnDelete(DeleteBehavior.NoAction);
+        });
 
     }
 }
