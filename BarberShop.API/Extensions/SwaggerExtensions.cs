80f3b74c3b855cdaf2923a09dc8f7f07015f6d85
EduardoHAnacleto <typhonedu@gmail.com>
2026-05-11 00:23:28 +1200

    Namespace quickfix

--- BarberShop.API/Extensions/SwaggerExtensions.cs
+++ BarberShop.API/Extensions/SwaggerExtensions.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace BarberShop.API.Extensions;
